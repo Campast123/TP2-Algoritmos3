b2f5e871c87fc3b28ee1d7bbf17c2fa53142536a
Martin Campanella <martin.campanella.93@gmail.com>
2016-06-23 00:57:41 -0300

    vistas y controller de ataques

---
+++ src/fiuba/algo3/controllers/MenuDeAtaqueController.java
@@ -0,0 +1,16 @@
+package fiuba.algo3.controllers;
+
+public class MenuDeAtaqueController {
+
+	public void clickBotonAtacarConPersonaje1(){
+		System.out.print("Atacar con personaje 1");
+	}
+
+	public void clickBotonAtacarConPersonaje2(){
+		System.out.print("Atacar con personaje 2");
+	}
+
+	public void clickBotonAtacarConPersonaje3(){
+		System.out.print("Atacar con personaje 3");
+	}
+}
